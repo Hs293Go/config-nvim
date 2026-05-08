9d8dd8807b6dbdb6b8ed0887b906e9d6793d54fa
H S Helson Go <hellston20a@gmail.com>
2026-05-08 15:56:35 -0400

    Cleanup session management to not remember terminals

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -2,7 +2,21 @@ return {
     {
         "folke/persistence.nvim",
         event = "BufReadPre",
-        opts = {},
+        opts = {
+            -- Wipe terminal and overseer buffers before mksession runs so they
+            -- don't get restored as empty no-name buffers next launch.
+            pre_save = function()
+                for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                    if vim.api.nvim_buf_is_valid(buf) then
+                        local bt = vim.bo[buf].buftype
+                        local ft = vim.bo[buf].filetype
+                        if bt == "terminal" or ft:match("^[Oo]verseer") then
+                            pcall(vim.api.nvim_buf_delete, buf, { force = true })
+                        end
+                    end
+                end
+            end,
+        },
         keys = {
             {
                 "<leader>qs",
